945cd944868692aed36b10f1295b09702650a0cd
Urmila Thakuri <134345714+Urmilalala@users.noreply.github.com>
2024-11-07 10:53:04 +0545

    adding view order features

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -6,7 +6,6 @@ import { validateMyRestaurantRequest } from "../middleware/validation";
 
 const router = express.Router();
 
-
 const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
@@ -14,6 +13,20 @@ const upload = multer({
     fileSize: 5 * 1024 * 1024, //5mb
   },
 });
+
+router.get(
+  "/order",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.getMyRestaurantOrders
+);
+
+router.patch(
+  "/order/:orderId/status",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateOrderStatus
+);
 
 router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
 
